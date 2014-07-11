1b21b3033591c68acda6dbd0af0ef71aaf2d1545
Sigotron <sigotron@gmail.com>
2014-07-11 11:16:31 -0700

    First Test Written

--- OrderEntryMockingPractice/Services/OrderService.cs
+++ OrderEntryMockingPractice/Services/OrderService.cs
@@ -6,6 +6,12 @@ namespace OrderEntryMockingPractice.Services
     {
         public OrderSummary PlaceOrder(Order order)
         {
+            // if not valid, throw exception why not valid
+                // order valid if, customer exists
+                // items are in stock
+
+            // else
+            // OrderSummary Returned
             return null;
         }
     }
--- OrderEntryMockingPracticeTests/OrderServiceTests.cs
+++ OrderEntryMockingPracticeTests/OrderServiceTests.cs
@@ -1,6 +1,24 @@
-﻿namespace OrderEntryMockingPracticeTests
+﻿
+
+
+using NUnit.Framework;
+using OrderEntryMockingPractice.Services;
+
+namespace OrderEntryMockingPracticeTests
 {
+    [TestFixture]
     public class OrderServiceTests
     {
+      
+        [Test]
+        public static void TestOrderSummaryDoesNotReturnNull()
+        {
+            //Arrange
+            OrderService orderService = new OrderService();
+            //Act
+            var result = orderService.PlaceOrder(null);
+            //Assert
+            Assert.That(result,!Is.EqualTo(null));
+        }
     }
 }
